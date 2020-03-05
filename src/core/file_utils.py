0519f85e507c9bbd35a4fee613e7d255628d325b
Juanma <lorenzoperez.juanmaria@gmail.com>
2020-03-05 11:38:31 +0100

    feature: Analisis diario
    Añadido el método para analizar periodicamente los archivos de todas las replicas. Los resultados se escriben  a logs-MONTH-YEAR.log

--- src/core/file_utils.py
+++ src/core/file_utils.py
@@ -2,97 +2,194 @@ import hashlib
 import json
 import os
 import shutil
+from functools import reduce
+from datetime import datetime
+from apscheduler.schedulers.background import BackgroundScheduler
+
+from flask.json import jsonify
 
 
-def file_generator(path, num_replicas, num_files):
+class FileUtils():
     """
-    Generate files and replicas
-
-    Arguments:
-        path {str} -- Base path to generate files
-        num_replicas {integer} -- Number of replicas
-        num_files {integer} -- Number of files
-
-    Returns:
-        dict -- Dict with files and hashes
+    Class for files processing
     """
-    path = os.path.join(path, "replicas")
-    # Delete all files
-    shutil.rmtree(path)
 
-    if not os.path.exists(path):
-        os.mkdir(path)
+    path = os.path.join(os.path.dirname(__file__), "..")
+    replicas_path = ""
+    hashes = {}
+    json_filename = os.path.join(path, "files.json")
+    
 
-    letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    num_letters = len(letters)
+    def __init__(self):
+        if not bool(self.hashes) and os.path.exists(self.json_filename):
+            with open(self.json_filename) as json_data:
+                self.hashes = json.load(json_data)
 
-    for i in range(num_replicas):
-        next = [""]
-        directorio = "replica" + str(i)
+    def get_hash(self, filename):
+        """
+        Read json with filenames and hashes
 
-        if not os.path.exists(os.path.join(path, directorio)):
-            os.mkdir(os.path.join(path, directorio))
+        Returns:
+            dict -- files and hashes
+        """
+        msg = ""
+        if bool(self.hashes):
+            replicas = self.hashes["replicas"]
+        else:
+            result = {"message": "HIDS failure. Please populate files."}, 500
 
-        for i in range(num_files):
-            with open(
-                    os.path.join(
-                        path,
-                        directorio,
-                        f"archivo{i}.txt"
-                    ), "w") as file:
+        try:
+            for replica in replicas:
+                if filename in replicas[replica]:
+                    result = {
+                        replica: {filename: replicas[replica][filename]}
+                    }, 200
+                    msg = ""
+                    break
+                else:
+                    msg += f"'{filename}' not exist in '{replica}'; "
+        except Exception as e:
+            if not result:
+                result = {"message": e.strerror}, 400
+        if msg != "":
+            result = {"message": msg}, 400
+        return result
 
-                if next[-1] == letters[-1]:
-                    next.append(letters[0])
-                for i in range(len(next)):
-                    if letters.index(next[i]) == num_letters - 1:
-                        next[i] = "a"
-                    else:
-                        next[i] = letters[letters.index(next[i]) + 1]
-                        break
+    def get_hashes(self):
+        """
+        Read json with filenames and hashes
 
-                file.write("".join(next))
+        Returns:
+            dict -- files and hashes
+        """
+        if bool(self.hashes):
+            result = self.hashes, 200
+        else:
+            result = {"message": "HIDS failure. Please populate files."}, 500
 
-    return files_hashes_to_dict(path)
+        return result
+
+    def files_hashes_to_dict(self, path):
+        """
+        Generate dictionary with files and hashes
+
+        Arguments:
+            path {str} -- Path files
+
+        Returns:
+            dict -- Dict with files and hashes
+        """
+        dir = {}
+        rootdir = path.rstrip(os.sep)
+        start = rootdir.rfind(os.sep) + 1
+        for path, dirs, files in os.walk(rootdir):
+            folders = path[start:].split(os.sep)
+            subdir = {
+                key: self._file_to_hash(
+                    os.path.join(path, key)
+                ) for key in files
+            }
+            parent = reduce(dict.get, folders[:-1], dir)
+            parent[folders[-1]] = subdir
+        with open(self.json_filename, 'w') as self.json_file:
+            json.dump(dir, self.json_file)
+        self.hashes = dir
+        return dir
+
+    def _file_to_hash(self, file):
+        """
+        Calculate file hash
+
+        Arguments:
+            file {str} -- Path of file
+
+        Returns:
+            integer -- hash
+        """
+        sha256_hash = hashlib.sha256()
+        with open(file, "rb") as f:
+            # Read and update hash string value in blocks of 4K
+            for byte_block in iter(lambda: f.read(4096), b""):
+                sha256_hash.update(byte_block)
+            hash_file = sha256_hash.hexdigest()
+        return hash_file
+
+    def file_generator(self, num_replicas, num_files):
+        """
+        Generate files and replicas
+
+        Arguments:
+            path {str} -- Base path to generate files
+            num_replicas {integer} -- Number of replicas
+            num_files {integer} -- Number of files
+
+        Returns:
+            dict -- Dict with files and hashes
+        """
+        self.replicas_path = os.path.join(self.path, "replicas")
+        # Delete all files
+        if os.path.exists(self.replicas_path):
+            shutil.rmtree(self.replicas_path)
+
+        # Create folder
+        os.mkdir(self.replicas_path)
+
+        letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        num_letters = len(letters)
+
+        for i in range(num_replicas):
+            next = [""]
+            directorio = "replica" + str(i)
+
+            if not os.path.exists(os.path.join(self.replicas_path, directorio)):
+                os.mkdir(os.path.join(self.replicas_path, directorio))
+
+            for i in range(num_files):
+                with open(
+                        os.path.join(
+                            self.replicas_path,
+                            directorio,
+                            f"archivo{i}.txt"
+                        ), "w") as file:
+
+                    if next[-1] == letters[-1]:
+                        next.append(letters[0])
+                    for i in range(len(next)):
+                        if letters.index(next[i]) == num_letters - 1:
+                            next[i] = "a"
+                        else:
+                            next[i] = letters[letters.index(next[i]) + 1]
+                            break
+
+                    file.write("".join(next))
+
+        return self.files_hashes_to_dict(self.replicas_path), 201
+
+    def daily_analysis(self):
+        fecha_actual = datetime.now()
+        file_logs = "logs-" + str(fecha_actual.month) + "-" + str(fecha_actual.year) + ".log"
+        file_logs_path = os.path.join(self.path, file_logs)
+        with open(file_logs_path, "a+", encoding="utf-8") as f:
+            f.write("================== Analysis of " + fecha_actual.now().strftime("%d/%m/%Y") + " ==================\n")
+            for replica in self.hashes["replicas"].keys():
+                dict_hashes_replica = self.hashes[replica]
+                for file_name in dict_hashes_replica.keys():
+                    hash_dict = dict_hashes_replica[file_name]
+                    hash_of_file_in_replica = self._file_to_hash(os.path.join(replica, file_name))
+                    if hash_dict != hash_of_file_in_replica:
+                        f.write("INTEGRITY ERROR (" + fecha_actual.now().strftime("%H:%M:%S %d/%m/%Y") + "): '" + file_name + "' (" + replica +")\n")
+            f.write("\n")        
+
+    def export_dict_json(self):
+        with open(self.json_filename, 'w') as json_file:
+            json.dump(self.hashes, json_file)
 
 
-def files_hashes_to_dict(path):
-    """
-    Generate dictionary with files and hashes
+    def start_scheduler(self):
+        scheduler = BackgroundScheduler()
 
-    Arguments:
-        path {str} -- Path files
+        # Para cambiar el intervalo, sustituir hours=24 por minutes= o seconds=
+        scheduler.add_job(self.daily_analysis, "interval", hours=24)
+        scheduler.add_job(self.export_dict_json, "interval", hours=1)
 
-    Returns:
-        dict -- Dict with files and hashes
-    """
-    d = {'name': os.path.basename(path)}
-    if os.path.isdir(path):
-        d['children'] = [
-            files_hashes_to_dict(os.path.join(path, x))
-            for x in os.listdir(path)
-        ]
-    else:
-        d['hash'] = _file_to_hash(path)
-
-    with open('files.json', 'w') as json_file:
-        json.dump(d, json_file)
-    return d
-
-
-def _file_to_hash(file):
-    """
-    Calculate file hash
-
-    Arguments:
-        file {str} -- Path of file
-
-    Returns:
-        integer -- hash
-    """
-    sha256_hash = hashlib.sha256()
-    with open(file, "rb") as f:
-        # Read and update hash string value in blocks of 4K
-        for byte_block in iter(lambda: f.read(4096), b""):
-            sha256_hash.update(byte_block)
-        hash_file = sha256_hash.hexdigest()
-    return hash_file
+        scheduler.start()
